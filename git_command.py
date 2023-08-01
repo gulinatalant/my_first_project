9d08591f54b308504042215e307f974a6cd14d10
Gulina <b.g.070917@gmail.com>
2023-08-01 19:28:15 +0600

    first commit

---
+++ git_command.py
@@ -0,0 +1,15 @@
+# git = это система управления версиями кода
+
+# github - сайт, который дает хранилище для кода
+# также есть gitlab, bitbucket
+
+# репозиторий - папка с проектомс которой может работать git
+
+# ssh-keygen - создание ключа
+
+# git config --global user.name "your-name"
+# git comfig --global user.email "your-eamil"
+#  указание своих данных
+
+# git init - превратить папку в репозиторий
+# создает скрытую папку .git
